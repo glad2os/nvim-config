f6d4311a2babf1d49945681240703d4bafa390f7
Ruslan Ibragimov <lite.console4bit@gmail.com>
2025-05-15 16:12:12 +0300

    Telescope install

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,12 @@ vim.keymap.set("n", "<leader>h", ":split<CR>", { desc = "Horizontal Split" })
 
 -- Save the file 
 vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file", silent = true })
+
+
+-- Telescope
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
