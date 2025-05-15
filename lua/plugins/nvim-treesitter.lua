929e949ea4aa1ddafd70e2d113c4f50650d8587a
Ruslan Ibragimov <lite.console4bit@gmail.com>
2025-05-15 16:05:55 +0300

    colours + fix

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,28 @@
 return {
-  { "nvim-treesitter/nvim-treesitter", opts = {} },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    ensure_installed = {
+      "bash",
+      "dockerfile",
+      "helm",
+      "yaml",
+      "lua",
+      "json",
+      "markdown",
+      "vim"
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
 
