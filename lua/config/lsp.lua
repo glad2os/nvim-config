fe8e5a403f99f7b403e00b3fb917efd93841e69a
glad2os <r.ibragimov@softlogic.ai>
2025-05-15 14:15:33 +0300

    helm package

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,20 +1,42 @@
 require("lspconfig").lua_ls.setup {
   settings = {
     Lua = {
-      runtime = {
-        version = "LuaJIT", -- or "Lua 5.1", etc.
-      },
-      diagnostics = {
-        globals = { "vim" }, -- Recognize `vim` as a global
-      },
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
       workspace = {
         library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false, -- Prevent unnecessary prompts
+        checkThirdParty = false,
       },
-      telemetry = {
-        enable = false,
-      },
+      telemetry = { enable = false },
     },
   },
+}
+
+
+require("lspconfig").helm_ls.setup {
+  settings = {
+    ["helm-ls"] = {
+      logLevel = "info",
+      valuesFiles = {
+        mainValuesFile = "values.yaml",
+        lintOverlayValuesFile = "values.lint.yaml",
+        additionalValuesFilesGlobPattern = "values*.yaml",
+      },
+      yamlls = {
+        enabled = true,
+        enabledForFilesGlob = "**/*.{yaml,yml}",
+        diagnosticsLimit = 50,
+        showDiagnosticsDirectly = false,
+        path = "yaml-language-server",
+        config = {
+          schemas = {
+            kubernetes = "templates/**",
+          },
+          completion = true,
+          hover = true,
+        }
+      }
+    }
+  }
 }
 
