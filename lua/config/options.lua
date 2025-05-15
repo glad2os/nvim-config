662dfe6a45d48eeb930812819972ff19f06c4889
Ruslan Ibragimov <lite.console4bit@gmail.com>
2025-05-15 14:15:33 +0300

    helm package

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@ vim.opt.shiftwidth = 2      -- Number of spaces to use for each step of (auto)in
 vim.opt.tabstop = 2         -- Number of spaces a <Tab> counts for
 vim.opt.expandtab = true    -- Use spaces instead of tabs
 
+
+vim.opt.number = true          -- Show absolute line numbers
+vim.opt.relativenumber = false -- (Optional) turn off relative numbers
+
