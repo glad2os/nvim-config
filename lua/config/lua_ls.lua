ff61e683a8fb350d7989d2ac4f656b69bf538754
Ruslan <lite.console4bit@gmail.com>
2025-10-02 11:14:45 +0300

    remove garbage

--- lua/config/lua_ls.lua
+++ lua/config/lua_ls.lua
@@ -1,20 +1,4 @@
---  vim.lsp.config({
---   settings = {
---     Lua = {
---       runtime = { version = "LuaJIT" },
---       diagnostics = { globals = { "vim" } },
---       workspace = {
---         library = vim.api.nvim_get_runtime_file("", true),
---         checkThirdParty = false,
---       },
---       telemetry = { enable = false },
---     },
---   },
--- })
---
--- -- Register Lua LS config
 vim.lsp.config('lua_ls', {
-  -- cmd = { 'lua-language-server' }, -- usually not needed if it's on $PATH
   filetypes = { 'lua' },
   root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
   settings = {
