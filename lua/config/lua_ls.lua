58dbb5ba467c1ced65edea27fafe0ed7e3e8cb14
Ruslan <lite.console4bit@gmail.com>
2025-10-02 11:12:17 +0300

    The require(lspconfig) "framework" is deprecated

--- lua/config/lua_ls.lua
+++ lua/config/lua_ls.lua
@@ -1,15 +1,30 @@
-require("lspconfig").lua_ls.setup {
+--  vim.lsp.config({
+--   settings = {
+--     Lua = {
+--       runtime = { version = "LuaJIT" },
+--       diagnostics = { globals = { "vim" } },
+--       workspace = {
+--         library = vim.api.nvim_get_runtime_file("", true),
+--         checkThirdParty = false,
+--       },
+--       telemetry = { enable = false },
+--     },
+--   },
+-- })
+--
+-- -- Register Lua LS config
+vim.lsp.config('lua_ls', {
+  -- cmd = { 'lua-language-server' }, -- usually not needed if it's on $PATH
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
   settings = {
     Lua = {
-      runtime = { version = "LuaJIT" },
-      diagnostics = { globals = { "vim" } },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
-      },
+      workspace = { checkThirdParty = false },
+      diagnostics = { globals = { 'vim' } },
       telemetry = { enable = false },
+      hint = { enable = true },
+      completion = { callSnippet = 'Replace' },
     },
   },
-}
-
+})
 
