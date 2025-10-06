95179a4eab151c7c4d254c722da7f364ffdf1a7e
Ruslan Ibragimov <lite.console4bit@gmail.com>
2025-10-06 17:35:11 +0300

    Update colorscheme.lua (#1)
    Implement transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,34 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("tokyonight-night")
+  opts = {
+    style = "night",
+    transparent = true,
+    styles = { sidebars = "transparent", floats = "transparent" },
+  },
+  config = function(_, opts)
+    vim.opt.termguicolors = true
+    require("tokyonight").setup(opts)
+    vim.cmd.colorscheme("tokyonight")   -- стиль берётся из opts.style
+
+    -- добиваем "плашки" от плагинов/окон
+    local function make_transparent()
+      local groups = {
+        "Normal","NormalNC","SignColumn","EndOfBuffer",
+        "LineNr","CursorLineNr","FoldColumn",
+        "NormalFloat","FloatBorder","WinSeparator","MsgArea",
+        -- популярные плагины
+        "TelescopeNormal","TelescopeBorder",
+        "NvimTreeNormal","NvimTreeNormalNC",
+      }
+      for _, g in ipairs(groups) do
+        pcall(vim.api.nvim_set_hl, 0, g, { bg = "none" })
+      end
+    end
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("TokyonightTransparent", { clear = true }),
+      callback = make_transparent,
+    })
+    make_transparent()
   end,
 }
-
